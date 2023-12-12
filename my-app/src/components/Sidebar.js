3adef3c5b9c238627468e0d3844ebae4d38d3482
BubiniB <cristina.ekqvist@outlook.com>
2023-12-12 12:05:17 +0000

    Add css to sidebar

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,6 +1,7 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
-const Sidebar = () => <div>
+const Sidebar = () => <div className = {css.sidebar}>
     <a href="#">My Photos</a>
     <a href="#">My Illustrations</a>
     <a href="#">My Paintings</a>
